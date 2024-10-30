f1c01d53f2cd88e9ffdbf3987344224efc413ecb
DESKTOP-KF3CTE4\inham <inham3796@gmail.com>
2024-10-29 22:57:40 -0300

    Deploy do build

---
+++ System-login/assets/index-4Wy5ixWh.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Bem-vindo ao Sistema de Login</h1>
+        </div>
+    );
+};
+
+export default App;
